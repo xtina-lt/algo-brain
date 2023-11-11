29e9c362f7417b4c4ed4104b747175de5524d2ef
WORK HARD💼 <xtina_lt@outlook.com>
2023-11-11 14:38:00 -0500

    contains duplicate

--- arrAlgos.js
+++ arrAlgos.js
@@ -34,22 +34,65 @@ const twoSum = (arr, target) => {
     }
 }
 
-// true = GO YOU, false = TRY AGAIN
-let example1 = twoSum([2,7,11,15], 9) 
-console.log( myFun.isSame(example1,[0, 1]) )
+// // true = GO YOU, false = TRY AGAIN
+// let example1 = twoSum([2,7,11,15], 9) 
+// console.log( myFun.isSame(example1,[0, 1]) )
 
-// Input: nums = [3,2,4], target = 6
-// Output: [1,2]
-let example2 = twoSum([3,2,4], 6)
-console.log( myFun.isSame(example2, [1,2]) )
+// // Input: nums = [3,2,4], target = 6
+// // Output: [1,2]
+// let example2 = twoSum([3,2,4], 6)
+// console.log( myFun.isSame(example2, [1,2]) )
 
-// Input: nums = [3,3], target = 6
-// Output: [0,1]
-let example3 = twoSum([3,3], 6)
-console.log( myFun.isSame(example3, [0,1]) )
+// // Input: nums = [3,3], target = 6
+// // Output: [0,1]
+// let example3 = twoSum([3,3], 6)
+// console.log( myFun.isSame(example3, [0,1]) )
 
 // ------------- end two sum ------------------
 
+
+
+
+
+// -------------- # 3 CONTAINS DUPLICATE ---------------------------
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+const containsDuplicate = arr => {
+    // // BRUTE FORCE
+    // let obj = {}
+    // for (let i=0; i<arr.length; i++){
+    //     if (obj[arr[i]] !== undefined) return true
+    //     obj[arr[i]] = 0
+    // }
+    // return false
+
+    // // GETTING WARMER
+    // let set = new Set(arr)
+    // return set.size !== arr.length
+
+    // // ONE LINER
+    return new Set(arr).size !== arr.length
+}
+
+
+// // Input: nums = [1,2,3,1]
+// // Output: true
+// let example1 = containsDuplicate([1,2,3,1]) 
+// console.log( myFun.isSame(example1, true) )
+
+// // Input: nums = [1,2,3,4]
+// // Output: false
+// let example2 = containsDuplicate([1,2,3,4])
+// console.log( myFun.isSame(example2, false) )
+
+// // Input: nums = [3,3], target = 6
+// // Output: [0,1]
+// let example3 = containsDuplicate([1,1,1,3,3,4,3,2,4,2])
+// console.log( myFun.isSame(example3, true) )
+
+// ------------- end contains duplicate ------------------
 
 
 
---
+++ playGround.js
@@ -0,0 +1,2 @@
+let str = "2023-11-10T12:34:56Z"
+console.log(Date(str))
