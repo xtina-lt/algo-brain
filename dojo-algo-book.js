dd326ec115167051ce4f9df0ff564c2da994f9d7
WORK HARD💼 <xtina_lt@outlook.com>
2023-11-15 19:52:28 -0500

    changed to simple

--- dojo-algo-book.js
+++ dojo-algo-book.js
@@ -3,25 +3,60 @@
 
 
 
-// Print 1-255
-const printNums = (start, stop)  => {
-    for (let i=start; i <= stop; i++)
-        console.log("Hello World", i)
+// 1 ) Print all the integers from 1 to 255.
+const printNums = () => {
+    let result = ""
+
+    for (let i = 1; i <= 255; i++) result += `${i}, `
+
+    console.log( result.substring(0, result.length - 2) )
 }
-printNums(1,3)
+// printNums()
+
+
+// 2 ) Print integers from 0 to 255, and with each integer print the sum so far.
+const printSum = () => {
+    let result = ""
+    let sum = 0
+
+    for (let i = 1; i <= 255; i++) {
+        result += `${i}, `
+        sum += i
+    }
+
+    console.log( result.substring(0, result.length - 2) )
+    console.log("The summation is: ", sum)
+}
+printSum()
 
 
 
-// Print all the integers from 1 to 255.
-// Print Sum 0-255
-// Print integers from 0 to 255, and with each integer print the sum so far.
-// Find and Print Max
+
+// 3 ) Find and Print Max
 // Given an array, find and print its largest element.
+const printMax = (arr) => {
+    if (arr.length < 1) return false
+    let result = arr[0]
+
+    for (let i of arr) {
+        if (i > result) result = i
+    }
+
+    return result
+}
+printMax([-500, 4, 9, 235])
+
 // Array with Odds
 // Create an array with all the odd integers between 1 and 255 (inclusive).
+
+
 // Greater Than Y
 // Given an array and a value Y, count and print the number of array values greater than Y.
+
+
 // Max, Min, Average
 // Given an array, print the max, min and average values for that array.
+
+
 // Swap String For Array Negative Values
 // Replace any negative array values with 'Dojo'.
