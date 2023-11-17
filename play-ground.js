f1648b4c698852c3772dee0c8a5af12c8a33cd71
WORK HARD💼 <xtina_lt@outlook.com>
2023-11-16 21:04:51 -0500

    longest merge

--- play-ground.js
+++ play-ground.js
@@ -1,2 +1,35 @@
-let str = "2023-11-10T12:34:56Z"
-console.log(Date(str))
+// let str = "2023-11-10T12:34:56Z"
+// console.log(Date(str))
+
+const mergeWhile = (x, y) => {
+    let result = ""
+    let longest = (x.length >= y.length) ? x.length : y.length
+    let i=0
+
+    while (i < longest) {
+        if (i < x.length) result += x[i]
+        if (i < y.length) result += y[i]
+        i++
+    }
+
+    return result
+}
+
+const mergeFor = (x, y) => {
+    let result = ""
+    let longest = (x.length >= y.length) ? x.length : y.length
+
+    for (let i=0; i < longest; i++) {
+        if (i < x.length) result += x[i]
+        if (i < y.length) result += y[i]
+    }
+
+    return result
+}
+
+// adbecf
+// adbc
+// adef
+// console.log(mergeWhile("abc", "def"))
+// console.log(mergeWhile("abc", "d"))
+// console.log(mergeWhile("a", "def"))
