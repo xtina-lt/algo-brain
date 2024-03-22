80f6e63bf55aebe45922eac8a72d9c557cd0df10
WORK HARD💼 <xtina_lt@outlook.com>
2024-03-21 20:15:00 -0400

    starting leetcode 1358

--- leet-code.js
+++ leet-code.js
@@ -20,6 +20,58 @@ const containsDup = arr => {
 // check( containsDup([1,2,3,4]) , false)
 // check( containsDup([1,1,1,3,3,4,3,2,4,2]) , true )
 
+/*
+1358. Number of Substrings Containing All Three Characters
+Given a string s consisting only of characters a, b and c.
 
+Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+
+ 
+
+Example 1:
+
+Input: s = "abcabc"
+Output: 10
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again). 
+Example 2:
+
+Input: s = "aaacb"
+Output: 3
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+Example 3:
+
+Input: s = "abc"
+Output: 1
+
+*/
+
+// const numOfSubstr= str => {
+//     let abc = new Set(["a", "b", "c"])
+//     let result = 0
+//     let start = 0
+//     let end = 2
+
+//     // while the end is not at the end
+//     // while the window is not more than 3 chars
+//     while (start < str.length/2) {
+//         console.log( str[start] )
+
+//         let temp = new Set([str[start], str[end-start], str[end]])
+//         console.log( temp, abc )
+
+//         if ( temp.size == abc.size) {
+//             result++
+//             console.log("same")
+//         }
+        
+        
+//         start++
+//         end++
+//     }
+    
+//     return result
+// }
+
+// numOfSubstr("aaacb")
 
 
